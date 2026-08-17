2123cdcbe9a8ff0604f91b20faacf947e8b80ae3
Mateusz Biel <matbiel@gmail.com>
2026-08-17 20:02:14 +0200

    Hide system bards

--- app/src/main/kotlin/com/example/capybara/MainActivity.kt
+++ app/src/main/kotlin/com/example/capybara/MainActivity.kt
@@ -13,10 +13,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val windowInsetsController =
+            WindowCompat.getInsetsController(window, window.decorView)
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
         actionBar?.hide()
         setContent {
             MaterialTheme {
