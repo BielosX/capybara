f9c660c3768faa20d07d9c0ce9fd7789348e6b5b
Mateusz Biel <matbiel@gmail.com>
2026-08-26 20:08:18 +0200

    Hide virtual joystick on multitouch

--- app/src/main/kotlin/com/example/capybara/two_dimensional/TwoDView.kt
+++ app/src/main/kotlin/com/example/capybara/two_dimensional/TwoDView.kt
@@ -17,6 +17,7 @@ class TwoDView(context: Context) : SurfaceView(context), SurfaceHolder.Callback 
   lateinit var worker: BackgroundThread
   var x: Int = 0
   var y: Int = 0
+  var firstPointerId: Int? = null
 
   init {
     holder.addCallback(this)
@@ -34,25 +35,41 @@ class TwoDView(context: Context) : SurfaceView(context), SurfaceHolder.Callback 
     worker.join()
   }
 
+  /*
+   https://developer.android.com/develop/ui/views/touch-and-input/gestures/multi
+  */
   @SuppressLint("ClickableViewAccessibility")
   override fun onTouchEvent(event: MotionEvent?): Boolean {
     val msg = Message.obtain()
     when (event?.action) {
       MotionEvent.ACTION_DOWN -> {
+        firstPointerId = event.getPointerId(0)
         msg.data.putParcelable("event", InputEvent.Pressed(event.x, event.y))
         worker.handler?.dispatchMessage(msg)
         return true
       }
       // https://developer.android.com/reference/android/view/MotionEvent#batching
       MotionEvent.ACTION_MOVE -> {
-        msg.data.putParcelable("event", InputEvent.Moved(event.x, event.y))
-        worker.handler?.dispatchMessage(msg)
+        if (firstPointerId != null && event.getPointerId(event.actionIndex) == firstPointerId) {
+          msg.data.putParcelable("event", InputEvent.Moved(event.x, event.y))
+          worker.handler?.dispatchMessage(msg)
+        }
+        return true
+      }
+      MotionEvent.ACTION_POINTER_UP -> {
+        if (firstPointerId != null && event.getPointerId(event.actionIndex) == firstPointerId) {
+          msg.data.putParcelable("event", InputEvent.Released)
+          worker.handler?.dispatchMessage(msg)
+          firstPointerId = null
+        }
         return true
       }
       MotionEvent.ACTION_UP -> {
-        msg.data.putParcelable("event", InputEvent.Released)
-        worker.handler?.dispatchMessage(msg)
-        return true
+        if (firstPointerId != null) {
+          msg.data.putParcelable("event", InputEvent.Released)
+          worker.handler?.dispatchMessage(msg)
+          firstPointerId = null
+        }
       }
     }
     return super.onTouchEvent(event)
